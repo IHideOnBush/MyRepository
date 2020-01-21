50ef3d786c8776ce11dc4b706885b2506575c50f
liuyabin <liuyabin0822@163.com>
2020-01-21 16:07:43 +0800

    hello  world

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+		 System.out.println("other  1");
+        System.out.println("Hello World");
+		   System.out.println("other  2");
+		  System.out.println("Hello World");
+		
+    }
+}
